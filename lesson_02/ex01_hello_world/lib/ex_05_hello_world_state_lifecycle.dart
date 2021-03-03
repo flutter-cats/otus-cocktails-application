b644553739e0f3391c771485b6567989392ca961
Ilya Virnik <ilya.virnik@gmail.com>
2021-03-03 16:43:10 +0300

    add lesson 02 examples

---
+++ lesson_02/ex01_hello_world/lib/ex_05_hello_world_state_lifecycle.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MyOwnWidget(),
+  );
+}
+
+class MyOwnWidget extends StatefulWidget {
+  @override
+  _MyOwnWidgetState createState() => _MyOwnWidgetState();
+}
+
+class _MyOwnWidgetState extends State<MyOwnWidget> {
+  bool _isShapeCircle;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+    _isShapeCircle = false;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('didChangeDependencies');
+  }
+
+  @override
+  void didUpdateWidget(MyOwnWidget oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print('didUpdateWidget');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    print('dispose');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => setState(() => _isShapeCircle = !_isShapeCircle),
+      child: Container(
+        decoration: BoxDecoration(
+          shape: _isShapeCircle ? BoxShape.circle : BoxShape.rectangle,
+          color: _isShapeCircle ? Colors.lime : Colors.lightGreen,
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          'Hello, world!',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.black, fontSize: 40),
+        ),
+      ),
+    );
+  }
+}
