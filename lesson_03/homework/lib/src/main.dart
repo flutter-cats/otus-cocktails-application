13ae692f9e512d4b634b7a047ef8c614aa839954
Azarov Serge <s.a.azarov@gmail.com>
2021-03-14 21:51:39 +0300

    hw_0. Добавляет main для демонстрации в консоли

---
+++ lesson_03/homework/lib/src/main.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:homework/adapters/cocktail.dart';
+import 'package:homework/mocks/cocktail.dart';
+
+void main(){
+	var cocktail = adaptCoctail(
+		json: mockCocktail,
+		lang: 'en',
+	);
+
+	print(cocktail);
+	print('--------------------------------------------------------');
+	print('cocktail.lang: ${cocktail.lang}');
+	print('cocktail.id: ${cocktail.id}');
+	print('cocktail.name: ${cocktail.name}');
+	print('cocktail.dateModified: ${cocktail.dateModified}');
+	print('cocktail.instructions ${cocktail.instructions}');
+	print('cocktail.glass: ${cocktail.glass}');
+	print('cocktail.thumbUrl: ${cocktail.thumbUrl}');
+	print('cocktail.videoUrl: ${cocktail.videoUrl}');
+	print('cocktail.ingradients:');
+	for(var ingredient in cocktail.ingredients) {
+		print('-- ingradient:');
+		print('---- ingradient.measure: ${ingredient.measure}');
+		print('---- ingradient.ingredient: ${ingredient.ingredient}');
+	}
+
+	print('--------------------------------------------------------');
+	cocktail.lang = 'de';
+	print('cocktail.lang: ${cocktail.lang}');
+	print('cocktail.id: ${cocktail.id}');
+	print('cocktail.name: ${cocktail.name}');
+	print('cocktail.dateModified: ${cocktail.dateModified}');
+	print('cocktail.instructions ${cocktail.instructions}');
+	print('cocktail.glass: ${cocktail.glass}');
+	print('cocktail.thumbUrl: ${cocktail.thumbUrl}');
+	print('cocktail.videoUrl: ${cocktail.videoUrl}');
+	print('cocktail.ingradients:');
+	for(var ingredient in cocktail.ingredients) {
+		print('-- ingradient:');
+		print('---- ingradient.measure: ${ingredient.measure}');
+		print('---- ingradient.ingredient: ${ingredient.ingredient}');
+	}
+}
