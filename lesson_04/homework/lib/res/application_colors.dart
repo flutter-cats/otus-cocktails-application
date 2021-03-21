c9bca513ce80f91b51f7efa9b16cb407b28a153a
Maks Yastrebov <maks@yastrebov.email>
2021-03-21 20:07:15 +0300

    some fix

--- lesson_04/homework/lib/res/application_colors.dart
+++ lesson_04/homework/lib/res/application_colors.dart
@@ -9,12 +9,3 @@ class ApplicationColors {
   static const itemRating = Color(0xff2A293A);
 }
 
-
-// static const id_title_color = Color(0xff848396);
-// static const cocktail_properties_title_color = Color(0xffEAEAEA);
-// static const cocktail_ingredient_title_color = Color(0xffB1AFC6);
-// static const cocktail_description_background_color = Color(0xff1A1927);
-// static const cocktail_instruction_background_color = Color(0xff201F2C);
-// static const cocktail_item_description_background = Color(0xff15151C);
-// static const cocktail_item_rating_background = Color(0xff2A293A);
-
