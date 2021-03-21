e40c5b9b08254d7aa47e124b7bf0f6d05956cd90
Azarov Serge <s.a.azarov@gmail.com>
2021-03-21 10:32:58 +0300

    hw_04. Добавляет страницу коктейля

--- lesson_04/homework/lib/main.dart
+++ lesson_04/homework/lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 
-import 'cocktail_detail_page.dart';
+import 'pages/cocktail_detail_page/cocktail_detail_page.dart';
 import 'models/src/repository/sync_cocktail_repository.dart';
 
 void main() {
   final cocktail = SyncCocktailRepository().getHomeworkCocktail();
-  runApp( MaterialApp(
+  runApp(MaterialApp(
     title: 'Flutter Demo',
     home: Material(child: CocktailDetailPage(cocktail)),
   ));
 }
-
-
