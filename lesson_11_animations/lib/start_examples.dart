2281e81a18672730828283fa8113f3cac79dcdcb
andrey.smirnov <asmirnov@inspector-cloud.com>
2021-04-06 00:38:20 +0300

    lesson 11

---
+++ lesson_11_animations/lib/start_examples.dart
@@ -0,0 +1,21 @@
+import 'package:lesson_11_animations/00_raw_animation.dart' as animations_00_0;
+import 'package:lesson_11_animations/00_scene_builder.dart' as animations_00_1;
+import 'package:lesson_11_animations/01_animated_opacity.dart' as animations_01;
+import 'package:lesson_11_animations/02_animated_cross_fade.dart'
+    as animations_02;
+import 'package:lesson_11_animations/03_animated_switcher.dart'
+    as animations_03;
+import 'package:lesson_11_animations/05_animated_container.dart'
+    as animations_05;
+import 'package:lesson_11_animations/10_animated_positioned_curves.dart'
+    as animations_10;
+import 'package:lesson_11_animations/11_tween_lerp.dart' as animations_11;
+import 'package:lesson_11_animations/12_color_tween_lerp.dart' as animations_12;
+import 'package:lesson_11_animations/13_tween_animated_builder.dart'
+    as animations_13;
+import 'package:lesson_11_animations/20_animated_positioned_custom_curve.dart'
+    as animations_20;
+
+void main() {
+  animations_00_0.main();
+}
