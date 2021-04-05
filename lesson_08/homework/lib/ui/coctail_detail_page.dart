772449333372d89e3af8a24e58677704d756216f
Pavel Mishagin <xstyle71@gmail.com>
2021-04-05 14:31:43 +0300

    Prepare to HomeWotk_8

--- lesson_08/homework/lib/ui/coctail_detail_page.dart
+++ lesson_08/homework/lib/ui/coctail_detail_page.dart
@@ -1,1 +1,65 @@
-//TODO: Добавить сюда экран из домашнего задания с урока 5
+import 'package:flutter/material.dart';
+
+import 'package:flutter/services.dart';
+
+import '../core/src/widjets/CoctailDetailsHeader.dart';
+import '../core/src/widjets/CoctailDetailsInfo.dart';
+import '../core/src/widjets/CoctailDetailsInstrictions.dart';
+import '../core/src/widjets/StarRatingWidhet.dart';
+import '../core/models.dart';
+
+class CocktailDetailPage extends StatelessWidget {
+  const CocktailDetailPage(
+    this.cocktail, {
+    Key key,
+  }) : super(key: key);
+
+  final Cocktail cocktail;
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceSize = MediaQuery.of(context).size;
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: SingleChildScrollView(
+              child: Container(
+                width: deviceSize.width,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 343,
+                      child: CoctailDetailsHeader(
+                        imageUrl: cocktail.drinkThumbUrl,
+                      ),
+                    ),
+                    Container(
+                      width: deviceSize.width,
+                      child: CoctailDetailsInfo(
+                        title: cocktail.name,
+                        id: cocktail.id,
+                        isFavorit: cocktail.isFavourite,
+                        category: cocktail.category,
+                        typeCoctail: cocktail.cocktailType,
+                        typeGlass: cocktail.glassType,
+                      ),
+                    ),
+                    CoctailDetailsIngridients(
+                      ingridens: cocktail.ingredients,
+                    ),
+                    CoctailDetailsInstrictions(cocktail.instruction),
+                    StarRatingWidjet(
+                      stars: 3,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ));
+  }
+}
