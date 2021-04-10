4ed0cc2e6a252a6f10ff89704793164768d67dbd
Maks Yastrebov <maks@yastrebov.email>
2021-04-10 11:51:04 +0300

    fix isAlcoholic field mapping

--- lesson_05/cocktail_db_http_using/lib/src/utils/mapping.dart
+++ lesson_05/cocktail_db_http_using/lib/src/utils/mapping.dart
@@ -4,11 +4,10 @@ import 'package:cocktail_db_http_using/src/dto/ingredient_dto.dart';
 extension IngredientFromDto on IngredientDto {
   Ingredient getIngredient() {
     return Ingredient(
-      id: this.id,
-      name: this.ingredient,
-      description: this.description,
-      ingredientType: this.type,
-      isAlcoholic: this.alcohol == 'true'
-    );
+        id: this.id,
+        name: this.ingredient,
+        description: this.description,
+        ingredientType: this.type,
+        isAlcoholic: this.alcohol == 'Yes');
   }
 }
