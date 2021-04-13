0899f8f5cd7bed92d68907824b9edb586f65456f
Vsevolod Krasnov <vsevolod.krasnov@gmail.com>
2021-04-13 19:45:45 +0300

    Lesson 12: refactoring

--- lesson_12/examples/lib/main.dart
+++ lesson_12/examples/lib/main.dart
@@ -1,12 +1,6 @@
-import 'package:examples/widgets/animated_gradient_example.dart';
 import 'package:flutter/material.dart';
 
-import 'widgets/blend_mode_example.dart';
-import 'widgets/dart_ui_widgets_example.dart';
-import 'widgets/hourglass_example.dart';
-import 'widgets/path_operation_example.dart';
-import 'widgets/save_layer_example.dart';
-import 'widgets/transform_example.dart';
+import 'widgets/custom_paint_example.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,16 +15,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      // home:CustomPaintExample(),
+      home: CustomPaintExample(),
       // home: BezierCurveExample(),
       // home: PaintExample(),
       // home: ClipPathExample(),
       // home: PathOperationExample(),
       // home: TransformExample(),
+      // home: SaveLayerExample(),
       // home: BlendModeExample(),
       // home: AnimatedGradientExample(),
       // home: HourglassExample(),
-      home: DartUiWidgetsExample(),
+      // home: DartUiWidgetsExample(),
     );
   }
 }
