c610f75795349a44242d18e9399850137e1ee963
Nikita <sna2021@narod.ru>
2021-05-06 19:21:48 +0300

    add lesson_15

---
+++ lesson_15_dialogs/lesson/lib/custom/sample_1.dart
@@ -0,0 +1,55 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class CustomDialogSample1 extends StatefulWidget {
+  @override
+  _CustomDialogSample1State createState() => _CustomDialogSample1State();
+}
+
+class _CustomDialogSample1State extends State<CustomDialogSample1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CustomDialog'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            TextButton(
+              onPressed: _show,
+              child: Text(
+                'show general dialog',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _show() {
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      barrierLabel: 'Dismiss',
+      transitionDuration: Duration(milliseconds: 800),
+      transitionBuilder: (context, animation, secondaryAnimation, child) {
+        return SlideTransition(
+          position: Tween(
+            begin: Offset(0, -1),
+            end: Offset(0, 0),
+          ).animate(animation),
+          child: child,
+        );
+      },
+      pageBuilder: (context, animation, secondaryAnimation) {
+        return AlertDialog(
+          title: Text('Title'),
+          content: Text('Content'),
+        );
+      },
+    );
+  }
+}
---
+++ lesson_15_dialogs/lesson/lib/material/4_menu.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class ScaffoldPopups extends StatefulWidget {
+  @override
+  _ScaffoldPopupsState createState() => _ScaffoldPopupsState();
+}
+
+class _ScaffoldPopupsState extends State<ScaffoldPopups> {
+  final _appBarKey = GlobalKey();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        key: _appBarKey,
+        title: GestureDetector(
+            onTap: () {
+              _showMenu(context);
+            },
+            child: Text('Scaffold Popups')),
+      ),
+      body: Builder(
+        builder: (context) {
+          return _buildBody(context);
+        },
+      ),
+    );
+  }
+
+  // Пример с использованием PopupMenuButton
+  Widget _buildBody(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (ctx, i) {
+        return ListTile(
+          title: Text('Items'),
+          trailing: PopupMenuButton(
+            child: Icon(Icons.more_vert),
+            onSelected: (value) {
+              print('onSelected:${value}');
+              //action
+            },
+            itemBuilder: (context) {
+              return [
+                PopupMenuItem(
+                    value: 'variant1',
+                    child: ListTile(
+                      title: Text('Variant 1'),
+                    )),
+                PopupMenuItem(
+                    value: 'variant2',
+                    child: ListTile(
+                      title: Text('Variant 2'),
+                    )),
+                CheckedPopupMenuItem(
+                  value: 'variant3',
+                  checked: true,
+                  child: Text('Variant 3'),
+                )
+              ];
+            },
+          ),
+        );
+      },
+      itemCount: 10,
+    );
+  }
+
+  // Пример кастомного отображения
+  void _showMenu(BuildContext context) {
+    final renderBox =
+        _appBarKey.currentState!.context.findRenderObject() as RenderBox;
+    final globalPosition = renderBox.localToGlobal(Offset.zero);
+    showMenu(
+        context: context,
+        position:
+            RelativeRect.fromLTRB(globalPosition.dx, globalPosition.dy, 0, 0),
+        items: [
+          PopupMenuItem(
+              value: 'variant2',
+              child: ListTile(
+                title: Text('Variant 2'),
+              )),
+        ]);
+  }
+}
