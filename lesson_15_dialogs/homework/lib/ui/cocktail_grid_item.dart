6e22244c242a850619a15935184889d519de5c0d
Pavel Mishagin <xstyle71@gmail.com>
2021-05-10 14:10:21 +0300

    Make lesson 15 to null safe megrate

--- lesson_15_dialogs/homework/lib/ui/cocktail_grid_item.dart
+++ lesson_15_dialogs/homework/lib/ui/cocktail_grid_item.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:homework/core/src/model/cocktail_definition.dart';
 
 class CocktailGridItem extends StatelessWidget {
-  const CocktailGridItem(this.cocktailDefinition,
-      {Key? key})
-      : super(key: key);
+  const CocktailGridItem(this.cocktailDefinition, {Key? key}) : super(key: key);
 
   static const double aspectRatio = 170 / 215;
 
@@ -20,16 +18,17 @@ class CocktailGridItem extends StatelessWidget {
         children: [
           DecoratedBox(
             decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                stops: [0.44,0.94],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Color.fromRGBO(14, 13, 19, 0),Color(0xFF0E0D13)]
-              )
-            ),
+                gradient: LinearGradient(
+                    stops: [0.44, 0.94],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color.fromRGBO(14, 13, 19, 0),
+                      Color(0xFF0E0D13)
+                    ])),
             position: DecorationPosition.foreground,
             child: Image.network(
-              cocktailDefinition.drinkThumbUrl ?? '',
+              cocktailDefinition.drinkThumbUrl,
               fit: BoxFit.cover,
             ),
           ),
@@ -39,7 +38,7 @@ class CocktailGridItem extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(cocktailDefinition.name ?? '',
+                  Text(cocktailDefinition.name,
                       style: Theme.of(context).textTheme.bodyText1),
                 ]),
           )
