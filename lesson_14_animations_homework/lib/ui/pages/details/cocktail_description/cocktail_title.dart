d18731b7f70f4aa5305a685cfdefc4e7f2f04dac
andrey.smirnov <asmirnov@inspector-cloud.com>
2021-05-12 11:11:09 +0300

    animations module homework

---
+++ lesson_14_animations_homework/lib/ui/pages/details/cocktail_description/cocktail_title.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CocktailTitle extends StatelessWidget {
+  final String cocktailTitle;
+  final bool isFavorite;
+
+  CocktailTitle({
+    required this.cocktailTitle,
+    required this.isFavorite,
+  });
+
+  @override
+  Widget build(BuildContext context) => Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            cocktailTitle,
+            style: Theme.of(context).textTheme.headline3,
+          ),
+          _getIsFavoriteIcon()
+        ],
+      );
+
+  ///
+  /// todo:
+  /// отрефакторить данный метод в пользу stateless widget с поддержкой
+  /// анимации scale down/scale up
+  ///
+  Widget _getIsFavoriteIcon() => isFavorite
+      ? IconButton(
+          icon: Icon(Icons.favorite, color: Colors.white),
+          onPressed: () {},
+        )
+      : IconButton(
+          icon: Icon(Icons.favorite_border, color: Colors.white),
+          onPressed: () {},
+        );
+}
