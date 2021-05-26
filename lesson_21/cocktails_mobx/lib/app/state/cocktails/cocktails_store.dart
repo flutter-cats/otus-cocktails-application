de4e7fa1b77a384382dbca6a109982a216bc03fc
Vsevolod Krasnov <vsevolod.krasnov@gmail.com>
2021-05-26 21:03:56 +0300

    Lesson 21: improving future examples

--- lesson_21/cocktails_mobx/lib/app/state/cocktails/cocktails_store.dart
+++ lesson_21/cocktails_mobx/lib/app/state/cocktails/cocktails_store.dart
@@ -10,6 +10,9 @@ part 'cocktails_store.g.dart';
 class CocktailsStore = _CocktailsStoreBase with _$CocktailsStore;
 
 abstract class _CocktailsStoreBase with Store implements Initable {
+  static ObservableFuture<Iterable<CocktailDefinition>> emptyResponse =
+      ObservableFuture.value([]);
+
   _CocktailsStoreBase(this.cocktailRepository, this._categoriesStore) {
     // Добавляем реакцию: при изменении выбранной категории автоматически запустится загрузка коктейлей
     _disposers.add(
@@ -26,6 +29,15 @@ abstract class _CocktailsStoreBase with Store implements Initable {
 
   final cocktails = ObservableList<CocktailDefinition>();
 
+  @observable
+  ObservableFuture<Iterable<CocktailDefinition>> _cocktailsFuture =
+      emptyResponse;
+
+  @computed
+  bool get hasResults =>
+      _cocktailsFuture != emptyResponse &&
+      _cocktailsFuture.status == FutureStatus.fulfilled;
+
   @override
   Future<void> init() {
     return loadCocktails();
@@ -37,13 +49,15 @@ abstract class _CocktailsStoreBase with Store implements Initable {
     }
   }
 
-  // Возвращаемый Future можно использовать в UI: отображать индикатор прогресса.
-  // Или же его можно положить в Observable<Future>.
   @action
   Future<void> loadCocktails() async {
-    final loadedCocktails =
-        await cocktailRepository.fetchCocktailsByCocktailCategory(
-            this._categoriesStore.selectedCategory);
+    // Альтернативный вариант: использовать в UI Future, который возвращается этим экшеном
+    final future = cocktailRepository.fetchCocktailsByCocktailCategory(
+      this._categoriesStore.selectedCategory,
+    );
+    _cocktailsFuture = ObservableFuture(future);
+
+    final loadedCocktails = await future;
 
     cocktails.clear();
     cocktails.addAll(loadedCocktails);
