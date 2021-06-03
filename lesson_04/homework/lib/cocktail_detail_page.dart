3a79131bb3d7e367140649efb0978cbf1a8db851
IriEfremova <iriefremova@gmail.com>
2021-06-03 21:11:54 +0500

    Restore

--- lesson_04/homework/lib/cocktail_detail_page.dart
+++ lesson_04/homework/lib/cocktail_detail_page.dart
@@ -1,36 +1,3 @@
-Skip to content
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@IriEfremova 
-IriEfremova
-/
-otus-cocktails-application
-forked from flutter-cats/otus-cocktails-application
-0
-0
-5
-Code
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-Settings
-otus-cocktails-application/lesson_04/homework/lib/cocktail_detail_page.dart
-@sna2021
-sna2021 Lesson 04 (flutter-cats#6)
-…
-Latest commit 74022dc on 15 Mar
- History
- 1 contributor
-22 lines (19 sloc)  827 Bytes
-  
 import 'package:flutter/material.dart';
 import 'package:homework/models/models.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -53,15 +20,3 @@ class CocktailDetailPage extends StatelessWidget {
     ///
   }
 }
-© 2021 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
