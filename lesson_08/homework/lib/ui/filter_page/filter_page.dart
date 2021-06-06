d64543fe9d41ec62b2bd343426967d1dc9fa3b82
yamaks <maks@yastrebov.email>
2021-06-06 23:09:12 +0300

    cocktailsGrid

--- lesson_08/homework/lib/ui/filter_page/filter_page.dart
+++ lesson_08/homework/lib/ui/filter_page/filter_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
+import 'package:cocktail/core/models.dart';
 import 'package:cocktail/core/src/model/cocktail_category.dart';
 import 'package:cocktail/res/application_colors.dart';
+import 'package:cocktail/ui/cocktail_detail_page/cocktail_detail_page.dart';
 import 'package:flutter/material.dart';
 
 class FilterPage extends StatefulWidget {
@@ -24,6 +28,7 @@ class _FilterPageState extends State<FilterPage> {
         children: [
           _searchString(size),
           _cocktailCategoryChips(),
+          _cocktailsGrid()
         ],
       )),
     );
@@ -92,5 +97,112 @@ class _FilterPageState extends State<FilterPage> {
     return Container(
         height: 50,
         child: ListView(scrollDirection: Axis.horizontal, children: chips));
+  }
+
+  Widget _cocktailsGrid() {
+    return FutureBuilder(
+        future: AsyncCocktailRepository()
+            .fetchCocktailsByCocktailCategory(_selectedCocktailCategory),
+        builder: (BuildContext context,
+            AsyncSnapshot<Iterable<CocktailDefinition>> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+                style: TextStyle(color: Colors.white),
+              ),
+            );
+          }
+
+          if (snapshot.hasData) {
+            return Expanded(
+                child: GridView.builder(
+                    shrinkWrap: true,
+                    padding: EdgeInsets.all(25),
+                    itemCount: snapshot.data.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 5,
+                        mainAxisSpacing: 8,
+                        childAspectRatio: 0.8),
+                    itemBuilder: (context, index) => Center(
+                          child: _cocktailTile(snapshot.data.elementAt(index)),
+                        )));
+          }
+
+          return Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+          );
+        });
+  }
+
+  Widget _cocktailTile(CocktailDefinition cocktail) {
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Stack(
+          children: [
+            Image(
+              width: 200,
+              height: 300,
+              image: NetworkImage(cocktail.drinkThumbUrl),
+              fit: BoxFit.fitHeight,
+            ),
+            Positioned(
+                bottom: 5,
+                left: 5,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      width: 150,
+                      padding: EdgeInsets.only(right: 10),
+                      child: Text(
+                        cocktail.name,
+                        style: TextStyle(color: Colors.white),
+                        softWrap: true,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 5),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: ApplicationColors.selectedBackgroundColor,
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                      child: Text('id: ${cocktail.id}',
+                          style: TextStyle(color: Colors.white, fontSize: 10)),
+                    )
+                  ],
+                )),
+            Material(
+                color: Colors.transparent,
+                child: InkWell(
+                  splashColor: Colors.blueGrey,
+                  onTap: () {
+                    _navigateToDetailPage(context, cocktail.id);
+                  },
+                )),
+          ],
+        ));
+  }
+
+  void _navigateToDetailPage(BuildContext context, String cocktailId) async {
+    Cocktail _cocktail;
+
+    try {
+      _cocktail =
+          await AsyncCocktailRepository().fetchCocktailDetails(cocktailId);
+
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => CocktailDetailPage(_cocktail)));
+    } catch (e) {
+      throw HttpException(e.toString());
+    }
   }
 }
