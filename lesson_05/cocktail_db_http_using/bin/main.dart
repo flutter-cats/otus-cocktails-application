bfb1f87a4765e920715c304eaed4ba6c2d564974
Pavel Korzhenko <pksmall@live.ru>
2021-06-08 19:07:01 +0300

    add ingredient_dto and begin refactoring

--- lesson_05/cocktail_db_http_using/bin/main.dart
+++ lesson_05/cocktail_db_http_using/bin/main.dart
@@ -14,7 +14,7 @@ Future<void> main() async {
   print(cocktail?.cocktailType);
   print(cocktail?.instruction);
   print('');
-  final ingredient = await repository.lookupIngredientById('11149');
+  final ingredient = await repository.lookupIngredientById('152');
   print(ingredient?.id);
   print(ingredient?.name);
   print(ingredient?.description);
