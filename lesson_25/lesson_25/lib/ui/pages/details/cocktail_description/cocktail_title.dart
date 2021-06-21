f350a269900f378a5c42ddee241252483f7e92e6
Andrey Smirnov <andreysmirnov@192.168.0.19>
2021-06-21 13:53:29 +0300

    25 lesson

--- lesson_25/lesson_25/lib/ui/pages/details/cocktail_description/cocktail_title.dart
+++ lesson_25/lesson_25/lib/ui/pages/details/cocktail_description/cocktail_title.dart
@@ -25,20 +25,6 @@ class CocktailTitle extends StatelessWidget {
         ],
       );
 
-  ///
-  /// todo:
-  /// отрефакторить данный метод в пользу отдельного widget с поддержкой
-  /// анимации scale down/scale up на 20% (curves по желанию)
-  ///
-  /// Новый виджет должен поддерживать два состояния -
-  /// isFavorite, !isFavorite (иконка filled, outline).
-  /// Иконка должна быть отрисована с использованием графических примитивов -
-  /// то есть использовать material icons нельзя,
-  /// работаем через CustomPaint/ClipPath.
-  /// При нажатии иконка должна увеличиваться на 20 % и
-  /// возвращаться в исходное состояние.
-  /// Можно добавить свою физику за счет Curves (ease, elastic, bounce).
-  ///
   Widget _getIsFavoriteIcon() => isFavorite
       ? IconButton(
           icon: Icon(Icons.favorite, color: Colors.white),
--- lesson_25/lesson_25/lib/ui/pages/filter_results_page.dart
+++ lesson_25/lesson_25/lib/ui/pages/filter_results_page.dart
@@ -88,16 +88,6 @@ class _FilterResultsPageWidgetState extends State<FilterResultsPageWidget> {
             );
           }
 
-          ///
-          /// todo:
-          /// отрефакторить использование CircularProgressIndicator
-          /// в пользу реализации своего кастомного виджета progress indicator
-          /// Этот виджет нужно реализовать самостоятельно,
-          /// используя стандартные средства Flutter для работы
-          /// с графическим canvas и средства анимации.
-          /// И затем переиспользовать вместо CircularProgressIndicator
-          /// (в местах отмеченных///todo:)
-          ///
           return SliverFillRemaining(
             child: Center(
               child: const CircularProgressIndicator(),
