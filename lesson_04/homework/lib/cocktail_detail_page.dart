db5de33b4d668e8b081800a1a04d7600ed240b5a
erslight <mhkyon@gmail.com>
2021-07-05 18:54:43 +0300

    added toolbar

--- lesson_04/homework/lib/cocktail_detail_page.dart
+++ lesson_04/homework/lib/cocktail_detail_page.dart
@@ -1,12 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:homework/models/models.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class CocktailDetailPage extends StatelessWidget {
   const CocktailDetailPage(
-      this.cocktail, {
-        Key key,
-      }) : super(key: key);
+    this.cocktail, {
+    Key key,
+  }) : super(key: key);
 
   final Cocktail cocktail;
 
@@ -17,6 +18,34 @@ class CocktailDetailPage extends StatelessWidget {
     /// https://www.figma.com/file/d2JJUBFu7Dg0HOV30XG7Z4/OTUS-FLUTTER.-%D0%A3%D1%80%D0%BE%D0%BA-3-%D0%94%D0%97?node-id=23%3A85
     ///для того чтобы весь контент поместился, необходимо использовать SingleChildScrollView()
     ///
-    ///
+    return Scaffold(
+        body: NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.white),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            actions: [
+              Padding(
+                padding: EdgeInsets.only(right: 20.0),
+                child: GestureDetector(
+                  onTap: () => {ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('meow')))},
+                  child: Icon(CupertinoIcons.share),
+                )
+              )
+            ],
+            expandedHeight: 300.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+                background:
+                    Image.network(cocktail.drinkThumbUrl, fit: BoxFit.cover)),
+          ),
+        ];
+      },
+      body: Center(child: Text('sample text')),
+    ));
   }
 }
