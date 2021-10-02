409b40853b5ff201e435c8c635698ced73d1098b
erslight <mhkyon@gmail.com>
2021-10-02 16:27:36 +0300

    added ingridients part

--- lesson_04/homework/lib/cocktail_detail_page.dart
+++ lesson_04/homework/lib/cocktail_detail_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:homework/models/models.dart';
@@ -20,43 +22,53 @@ class CocktailDetailPage extends StatelessWidget {
     ///
     return Scaffold(
         body: NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-        return <Widget>[
-          SliverAppBar(
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back, color: Colors.white),
-              onPressed: () => Navigator.of(context).pop(),
-            ),
-            actions: [
-              Padding(
-                  padding: EdgeInsets.only(right: 20.0),
-                  child: GestureDetector(
-                    onTap: () => {
-                      ScaffoldMessenger.of(context)
-                          .showSnackBar(SnackBar(content: Text('meow')))
-                    },
-                    child: Icon(CupertinoIcons.share),
-                  ))
-            ],
-            expandedHeight: 300.0,
-            floating: false,
-            pinned: true,
-            flexibleSpace: FlexibleSpaceBar(
-                background:
-                    Image.network(cocktail.drinkThumbUrl, fit: BoxFit.cover)),
-          ),
-        ];
-      },
-      body: Column(
-        children: [_buildCoctailDetails()],
-      ),
-    ));
+            headerSliverBuilder: (BuildContext context,
+                bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  leading: IconButton(
+                    icon: Icon(Icons.arrow_back,
+                        color: Colors.white),
+                    onPressed: () =>
+                        Navigator.of(context).pop(),
+                  ),
+                  actions: [
+                    Padding(
+                        padding:
+                            EdgeInsets.only(right: 20.0),
+                        child: GestureDetector(
+                          onTap: () => {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(SnackBar(
+                                    content: Text('meow')))
+                          },
+                          child: Icon(CupertinoIcons.share),
+                        ))
+                  ],
+                  expandedHeight: 300.0,
+                  floating: false,
+                  pinned: true,
+                  flexibleSpace: FlexibleSpaceBar(
+                      background: Image.network(
+                          cocktail.drinkThumbUrl,
+                          fit: BoxFit.cover)),
+                ),
+              ];
+            },
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  _buildCoctailDetails(),
+                  _buildCoctailIngridients()
+                ],
+              ),
+            )));
   }
 
   Container _buildCoctailDetails() {
     return Container(
       width: double.infinity,
-      height: 322,
+      height: 300,
       color: Colors.black87,
       child: Stack(
         children: [
@@ -79,11 +91,14 @@ class CocktailDetailPage extends StatelessWidget {
               top: 16,
               width: 38,
               height: 34,
-              child: SvgPicture.asset('assets/images/icon_hart.svg')),
+              child: SvgPicture.asset(
+                  'assets/images/icon_hart.svg')),
           _getCoctailDescriptionWidget(
-              104, 'Категория коктейля', cocktail.category.value),
-          _getCoctailDescriptionWidget(
-              176, 'Тип коктейля', cocktail.cocktailType.value),
+              104,
+              'Категория коктейля',
+              cocktail.category.value),
+          _getCoctailDescriptionWidget(176, 'Тип коктейля',
+              cocktail.cocktailType.value),
           _getCoctailDescriptionWidget(
               240, 'Тип стекла', cocktail.glassType.value)
         ],
@@ -91,12 +106,14 @@ class CocktailDetailPage extends StatelessWidget {
     );
   }
 
-  Widget _getCoctailDescriptionWidget(double marginTop, String title, String description) {
+  Widget _getCoctailDescriptionWidget(
+      double marginTop, String title, String description) {
     return Positioned(
         left: 16,
         top: marginTop,
         child: Stack(children: [
-          Text(title, style: TextStyle(color: Colors.green)),
+          Text(title,
+              style: TextStyle(color: Colors.green)),
           Container(
               padding: EdgeInsets.only(left: 16, top: 24),
               child: Text(
@@ -104,5 +121,59 @@ class CocktailDetailPage extends StatelessWidget {
                 style: TextStyle(color: Colors.red),
               ))
         ]));
+  }
+
+  Widget _buildCoctailIngridients() {
+    return Container(
+      color: Colors.black,
+      width: double.infinity,
+      child: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 24),
+            child: 
+          Align(
+            child: Text(
+              'Ингридиенты:',
+              style: TextStyle(color: Colors.purple),
+            ),
+            alignment: Alignment.center,
+          )
+          ),
+          _buildIngridientsTable(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildIngridientsTable() {
+    var meow = <TableRow>[];
+    cocktail.ingredients.forEach((ingridient) {
+      meow.add(TableRow(
+        children: [
+          Padding(padding: EdgeInsets.only(top: 16), 
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(ingridient.ingredientName,
+                style: TextStyle(
+                    color: Colors.white,
+                    decoration: TextDecoration.underline)),
+          )),
+          Padding(padding: EdgeInsets.only(top: 16),
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: Text(ingridient.measure,
+                style: TextStyle(color: Colors.white)),
+          )),
+        ],
+      ));
+      
+    });
+
+    var ingridientsTable = Table(children: meow);
+
+    return Padding(
+        child: ingridientsTable,
+        padding: EdgeInsets.fromLTRB(16, 48, 16, 24));
   }
 }
