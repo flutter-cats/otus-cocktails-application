c8cd07868850124241ad0ad81db260a80bd785bb
berthold <palenichev@gmail.com>
2021-10-07 09:05:38 +0300

    done hw5

--- lesson_05/cocktail_db_http_using/bin/main.dart
+++ lesson_05/cocktail_db_http_using/bin/main.dart
@@ -1,12 +1,11 @@
-import 'package:cocktail_db_http_using/models.dart';
 import 'package:cocktail_db_http_using/src/repository/app_repository.dart';
 
 Future<void> main() async {
   // final repository = AsyncCocktailRepository();
-  // final ingredient = await repository.lookupIngredientById('552');
-  // print(ingredient?.description);
+  // final popular = await repository.fetchPopularCocktails();
+  // print(popular.length);
 
   final repository = AppRepository();
-  final coctails = await repository.getCoctails();
-  // print(coctails.drinks.map((e) => e.strDrink).toList().toString());
+  final ingredient = await repository.getIngredientById('552');
+  print(ingredient);
 }
--- lesson_05/cocktail_db_http_using/lib/src/repository/coctail_api_service.dart
+++ lesson_05/cocktail_db_http_using/lib/src/repository/coctail_api_service.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
+import 'package:cocktail_app_models/models.dart';
 import 'package:cocktail_db_http_using/src/dto/coctail_response_dto.dart';
+import 'package:cocktail_db_http_using/src/dto/ingredient_reponse_dto.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart' ;
 
@@ -11,5 +13,17 @@ abstract class CoctailApiService {
   factory CoctailApiService(Dio dio, {String baseUrl}) = _CoctailApiService;
 
   @GET("/popular.php")
-  Future<CoctailReponseDto> getMeow();
+  Future<CoctailReponseDto> getPopularCoctails();
+
+  @GET("/random.php")
+  Future<CoctailReponseDto> getRandomCoctail();
+
+  @GET("/lookup.php")
+  Future<CoctailReponseDto> getCoctailDetails(@Query('i') String id);
+
+  @GET("/filter.php")
+  Future<CoctailReponseDto> getCoctailsByType(@Query("a") String type);
+
+  @GET("lookup.php")
+  Future<IngredientResponseDto> getIngredientById(@Query("iid") String id);
 }
