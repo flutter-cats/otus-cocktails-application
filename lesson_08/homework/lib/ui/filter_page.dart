1b445b881430454d42f0cdfd079379e7d0083455
berthold <palenichev@gmail.com>
2021-10-10 00:13:18 +0300

    added future builder form depends on request

--- lesson_08/homework/lib/ui/filter_page.dart
+++ lesson_08/homework/lib/ui/filter_page.dart
@@ -7,15 +7,96 @@ import 'package:flutter/material.dart';
 
 import 'filter_chip_list.dart';
 
-class CocktailsFilterScreen extends StatelessWidget {
+class CocktailsFilterScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CocktailsFilterScreenState();
+  }
+}
+
+class _CocktailsFilterScreenState extends State<CocktailsFilterScreen> {
+  final repository = AsyncCocktailRepository();
+
+  final categories = CocktailCategory.values;
+  var selectedCategories = <String>[];
+  var searchText = "";
+  var isFirstInit = true;
+
+  Future<Iterable<CocktailDefinition>>? categoryFuture;
+
+  @override
+  void setState(VoidCallback fn) {
+    if (!isFirstInit) {
+      super.setState(fn);
+    }
+    if (selectedCategories.isNotEmpty) {
+      categoryFuture =
+          repository.fetchCocktailsByCategories(selectedCategories);
+    } else {
+      categoryFuture = null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final categories = CocktailCategory.values;
     return Column(children: [
-      SearchField((text) => {print(text)}),
+      SearchField((text) => {print(text), searchText = text}),
       FilterChipGroup(
           items: categories.map((e) => e.value).toList(),
-          onSelectionChanged: (values) => {print(values)})
+          onSelectionChanged: (values) => {
+                print(values),
+                if (!isFirstInit)
+                  {toggleState(values)}
+                else
+                  {isFirstInit = false}
+              }),
+      selectedCategories.isNotEmpty
+          ? _buildListResult(categoryFuture!)
+          : Center(
+              heightFactor: 24,
+              child: Text('Нет результатов',
+                  style: TextStyle(color: Colors.white)))
     ]);
+  }
+
+  void toggleState(List<String> elements) {
+    selectedCategories.clear();
+    if (elements.isNotEmpty) {
+      selectedCategories.addAll(elements);
+    }
+    setState(() {});
+  }
+
+  FutureBuilder<Iterable<CocktailDefinition>> _buildListResult(
+      Future<Iterable<CocktailDefinition>> future) {
+    return FutureBuilder<Iterable<CocktailDefinition>>(
+        future: future,
+        builder: (context, snapshot) {
+          List<Widget> children;
+          if (snapshot.hasData) {
+            children = <Widget>[
+              Text('here is data: ${snapshot.requireData.first.name}',
+                  style: TextStyle(color: Colors.white))
+            ];
+          } else if (snapshot.hasError) {
+            children = [
+              Text('Сообщение об ошибке', style: TextStyle(color: Colors.white))
+            ];
+          } else {
+            children = [
+              Align(
+                  alignment: Alignment.center,
+                  child:
+                      Text('Поиск...', style: TextStyle(color: Colors.white)))
+            ];
+          }
+
+          return Center(
+              heightFactor: 20,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: children));
+        });
   }
 }
