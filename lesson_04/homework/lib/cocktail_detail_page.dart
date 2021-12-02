b11572ffe605835434b1ce5386341eb49adca10c
Gnutovv <gnutovv@gmail.com>
2021-12-02 08:41:09 +0300

    Start HW

--- lesson_04/homework/lib/cocktail_detail_page.dart
+++ lesson_04/homework/lib/cocktail_detail_page.dart
@@ -4,9 +4,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class CocktailDetailPage extends StatelessWidget {
   const CocktailDetailPage(
-      this.cocktail, {
-        Key key,
-      }) : super(key: key);
+    this.cocktail, {
+    Key key,
+  }) : super(key: key);
 
   final Cocktail cocktail;
 
@@ -17,6 +17,8 @@ class CocktailDetailPage extends StatelessWidget {
     /// https://www.figma.com/file/d2JJUBFu7Dg0HOV30XG7Z4/OTUS-FLUTTER.-%D0%A3%D1%80%D0%BE%D0%BA-3-%D0%94%D0%97?node-id=23%3A85
     ///для того чтобы весь контент поместился, необходимо использовать SingleChildScrollView()
     ///
-    ///
+    return Scaffold(
+      body: Text('Start HW'),
+    );
   }
 }
