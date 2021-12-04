97c5192cf80ed48cbd96739b31cf41270d868ab9
Gnutovv <gnutovv@gmail.com>
2021-12-04 12:00:23 +0300

    Finish HW_5

--- lesson_05/cocktail_db_http_using/bin/main.dart
+++ lesson_05/cocktail_db_http_using/bin/main.dart
@@ -4,4 +4,7 @@ Future<void> main() async {
   final repository = AsyncCocktailRepository();
   final cocktail = await repository.getRandomCocktail();
   print(cocktail);
+  // Мы получили объект Coctail с данными.
+  // Далее вызываю метод, который выведет в консоль описание каждого из составных ингридиентов.
+  if (cocktail != null) repository.showDiscriptionOfIngredients(cocktail);
 }
