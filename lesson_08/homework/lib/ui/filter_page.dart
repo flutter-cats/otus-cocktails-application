3032c8c830dd998795c0e5af9dabc25ae3ad831c
Gnutovv <gnutovv@gmail.com>
2021-12-10 20:13:58 +0300

    refactor в соответствии с архитектурой

--- lesson_08/homework/lib/ui/filter_page.dart
+++ lesson_08/homework/lib/ui/filter_page.dart
@@ -1,6 +1,7 @@
 // TODO: Сделать экран Фильтр по категории
 // Ссылка на макет: https://www.figma.com/file/Uzn5jHYiiFgacPCWNhwbc5/%D0%9A%D0%BE%D0%BA%D1%82%D0%B5%D0%B9%D0%BB%D0%B8-Copy?node-id=20%3A590
 
+import 'package:cocktail/core/models.dart';
 import 'package:cocktail/core/src/globals.dart';
 import 'package:cocktail/ui/search_widgets/category_list_view.dart';
 import 'package:cocktail/ui/search_widgets/res/consts.dart';
@@ -9,8 +10,16 @@ import 'package:cocktail/ui/search_widgets/search_error.dart';
 import 'package:cocktail/ui/search_widgets/search_loading.dart';
 import 'package:cocktail/ui/search_widgets/search_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-class CocktailsFilterScreen extends StatelessWidget {
+class CocktailsFilterScreen extends StatefulWidget {
+  @override
+  State<CocktailsFilterScreen> createState() => _CocktailsFilterScreenState();
+}
+
+class _CocktailsFilterScreenState extends State<CocktailsFilterScreen> {
+  CocktailCategory chosenCategory = CocktailCategory.values.first;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +28,22 @@ class CocktailsFilterScreen extends StatelessWidget {
         child: Column(
           children: [
             SearchTextField(),
-            CategoryListView(),
-            SearchCoctailsGrid(GetCoctails()
-                .getListOfCoctailFromChoisedCategories(
-                    Globals.chosenCategories)),
+            Container(
+              height: 46,
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: [
+                  for (CocktailCategory category in CocktailCategory.values)
+                    getCategoryItem(category, category == chosenCategory, () {
+                      super.setState(() => chosenCategory = category);
+                    }),
+                ],
+              ),
+            ),
+            // SearchCoctailsGrid(GetCoctails()
+            //     .getListOfCoctailFromChoisedCategories(
+            //         Globals.chosenCategories)),
             // SearchLoadingScreen(),
             // SearchError("передал ошибку"),
           ],
--- lesson_08/homework/lib/ui/search_widgets/category_list_view.dart
+++ lesson_08/homework/lib/ui/search_widgets/category_list_view.dart
@@ -1,100 +1,32 @@
-import 'package:cocktail/core/models.dart';
-import 'package:cocktail/core/src/globals.dart';
+import 'package:cocktail/core/src/model/cocktail_category.dart';
 import 'package:cocktail/ui/search_widgets/res/consts.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-final refreshListOfCategoies = GlobalKey();
-
-class CategoryListView extends StatefulWidget {
-  const CategoryListView({Key? key}) : super(key: key);
-  @override
-  _CategoryListViewState createState() => _CategoryListViewState();
-}
-
-class _CategoryListViewState extends State<CategoryListView> {
-  final catgories = CocktailCategory.values;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 80,
-      padding: const EdgeInsets.all(8.0),
-      child: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [
-          ...listOfItems(catgories),
-        ],
-      ),
-    );
-  }
-}
-
-class ListViewCategoryItem extends StatefulWidget {
-  final String catValue;
-  final String catName;
-  bool isActive = false;
-  ListViewCategoryItem(this.catValue, this.catName, this.isActive, {Key? key})
-      : super(key: key);
-
-  @override
-  State<ListViewCategoryItem> createState() => _ListViewCategoryItemState();
-}
-
-class _ListViewCategoryItemState extends State<ListViewCategoryItem> {
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          widget.isActive = !widget.isActive;
-          if (widget.isActive)
-            Globals.chosenCategories.add(widget.catName);
-          else
-            Globals.chosenCategories.remove(widget.catName);
-          print("Tap on kontainer ${widget.catName} \n Выбраны категории:");
-          Globals.chosenCategories.forEach((element) {
-            print(element);
-          });
-        });
-      },
-      child: Container(
-        margin: EdgeInsets.all(6),
-        height: 46,
-        padding: EdgeInsets.symmetric(horizontal: 24),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(26),
-          color: widget.isActive
-              ? activeCategoryContainerColor
-              : notActiveCategoryContainerColor,
-          border: Border.all(
-            color: borderColor,
-          ),
-        ),
-        alignment: Alignment.center,
-        child: Text(
-          widget.catValue,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 16,
-          ),
+Widget getCategoryItem(
+    CocktailCategory category, bool isActive, dynamic triggerSetState) {
+  return GestureDetector(
+    onTap: () => triggerSetState(),
+    child: Container(
+      margin: EdgeInsets.all(6),
+      height: 46,
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(26),
+        color: isActive
+            ? activeCategoryContainerColor
+            : notActiveCategoryContainerColor,
+        border: Border.all(
+          color: borderColor,
         ),
       ),
-    );
-  }
-}
-
-List<Widget> listOfItems(Iterable categories) {
-  List<Widget> listOfItems = [];
-  categories.forEach((category) {
-    listOfItems.add(ListViewCategoryItem(
+      alignment: Alignment.center,
+      child: Text(
         category.value,
-        category.name,
-        (Globals.chosenCategories.contains(category.name) ||
-                (Globals.chosenCategories.isEmpty && listOfItems.isEmpty))
-            ? true
-            : false));
-  });
-
-  return listOfItems;
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+        ),
+      ),
+    ),
+  );
 }
