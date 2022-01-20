b45fcc9c39fdcf7aa68e4aedaebebecd243ef0bb
Ilia Prokhorov <demensdeum@gmail.com>
2022-01-20 22:45:35 +0300

    HW5. Done

--- lesson_16_dialogs/homework/lib/ui/cocktail_grid_item.dart
+++ lesson_16_dialogs/homework/lib/ui/cocktail_grid_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:homework/core/models.dart';
 import 'package:homework/core/src/model/cocktail_definition.dart';
+import 'cocktail_detail_page.dart';
 
 class CocktailGridItem extends StatelessWidget {
   const CocktailGridItem(this.cocktailDefinition,
@@ -10,41 +12,60 @@ class CocktailGridItem extends StatelessWidget {
 
   final CocktailDefinition cocktailDefinition;
 
+  Future<void> openDetailsScreen(BuildContext context) async {
+
+    final repository = AsyncCocktailRepository();
+    final cocktail = await repository.fetchCocktailDetails(cocktailDefinition.id);
+
+    Navigator.push(context, MaterialPageRoute<bool>(
+    builder: (BuildContext context) {
+      return CocktailDetailPage(cocktail!, key: UniqueKey());
+    }
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(8),
-      child: Stack(
-        alignment: Alignment.center,
-        fit: StackFit.expand,
-        children: [
-          DecoratedBox(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                stops: [0.44,0.94],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [Color.fromRGBO(14, 13, 19, 0),Color(0xFF0E0D13)]
+    return
+      GestureDetector(
+        onTap: () {
+          openDetailsScreen(context);
+        },
+        child:
+          ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Stack(
+            alignment: Alignment.center,
+            fit: StackFit.expand,
+            children: [
+              DecoratedBox(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    stops: [0.44,0.94],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Color.fromRGBO(14, 13, 19, 0),Color(0xFF0E0D13)]
+                  )
+                ),
+                position: DecorationPosition.foreground,
+                child: Image.network(
+                  cocktailDefinition.drinkThumbUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(cocktailDefinition.name,
+                          style: Theme.of(context).textTheme.bodyText1),
+                    ]),
               )
-            ),
-            position: DecorationPosition.foreground,
-            child: Image.network(
-              cocktailDefinition.drinkThumbUrl,
-              fit: BoxFit.cover,
-            ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(cocktailDefinition.name,
-                      style: Theme.of(context).textTheme.bodyText1),
-                ]),
-          )
-        ],
-      ),
+        )
     );
   }
 }
