a5635f428a64c985e1266fbf2635938205d5a56d
Igor Kim <igor.skh@gmail.com>
2022-02-23 20:54:08 +0100

    add example for lookupIngredientById in main.dart

--- lesson_05/cocktail_db_http_using/bin/main.dart
+++ lesson_05/cocktail_db_http_using/bin/main.dart
@@ -4,4 +4,12 @@ Future<void> main() async {
   final repository = AsyncCocktailRepository();
   final cocktail = await repository.getRandomCocktail();
   print(cocktail);
+
+
+  List<String> ingredients = ["2", "552"];
+
+  ingredients.forEach((iid) async {
+    final ingredient = await repository.lookupIngredientById(iid);
+    print(ingredient);
+  });
 }
