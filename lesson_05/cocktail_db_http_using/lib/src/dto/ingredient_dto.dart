bdb5e45d2bc4ad28d20beda9a1952703dbbbbfc6
Igor Kim <igor.skh@gmail.com>
2022-02-23 20:53:19 +0100

    create cocktail ingredient DTO

---
+++ lesson_05/cocktail_db_http_using/lib/src/dto/ingredient_dto.dart
@@ -0,0 +1,25 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'ingredient_dto.g.dart';
+
+@JsonSerializable(createFactory: true, createToJson: false)
+class IngredientDto {
+  String? idIngredient;
+  String? strIngredient;
+  String? strDescription;
+  String? strType;
+  String? strAlcohol;
+  String? strABV;
+
+  IngredientDto(
+    this.idIngredient,
+    this.strIngredient,
+    this.strDescription,
+    this.strType,
+    this.strAlcohol,
+    this.strABV,
+  );
+
+  factory IngredientDto.fromJson(Map<String, dynamic> json) =>
+      _$IngredientDtoFromJson(json);
+}
