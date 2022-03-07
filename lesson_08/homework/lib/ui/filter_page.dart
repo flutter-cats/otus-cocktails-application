30f63e558e0a7a5fff85de8af51a1a3236ac4651
Ivan Bacharnikov <cjba4ar@gmail.com>
2022-03-07 21:08:03 +0500

    fix for new Flutter SDK

--- lesson_08/homework/lib/ui/filter_page.dart
+++ lesson_08/homework/lib/ui/filter_page.dart
@@ -1,13 +1,34 @@
 // TODO: Сделать экран Фильтр по категории
 // Ссылка на макет: https://www.figma.com/file/Uzn5jHYiiFgacPCWNhwbc5/%D0%9A%D0%BE%D0%BA%D1%82%D0%B5%D0%B9%D0%BB%D0%B8-Copy?node-id=20%3A590
 
+import 'package:cocktail/ui/category_selector.dart';
 import 'package:flutter/material.dart';
 
 class CocktailsFilterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Implement me'),
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {},
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                  filled: true,
+                  hintText: "Введите название коктейля",
+              ),
+            ),
+            CategorySelector(),
+          ],
+        ),
+      ),
     );
   }
 }
