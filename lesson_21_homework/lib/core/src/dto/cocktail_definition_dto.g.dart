aa13a0e70b921bd8438db85188b68281aa33901e
Igor Kim <igor.skh@gmail.com>
2022-04-29 12:23:02 +0200

    update dto, implement conversion

--- lesson_21_homework/lib/core/src/dto/cocktail_definition_dto.g.dart
+++ lesson_21_homework/lib/core/src/dto/cocktail_definition_dto.g.dart
@@ -7,10 +7,17 @@ part of 'cocktail_definition_dto.dart';
 // **************************************************************************
 
 CocktailDefinitionDto _$CocktailDefinitionDtoFromJson(
-    Map<String, dynamic> json) {
-  return CocktailDefinitionDto(
-    json['strDrink'] as String,
-    json['strDrinkThumb'] as String,
-    json['idDrink'] as String,
-  );
-}
+        Map<String, dynamic> json) =>
+    CocktailDefinitionDto(
+      json['strDrink'] as String,
+      json['strDrinkThumb'] as String,
+      json['idDrink'] as String,
+    );
+
+Map<String, dynamic> _$CocktailDefinitionDtoToJson(
+        CocktailDefinitionDto instance) =>
+    <String, dynamic>{
+      'strDrink': instance.strDrink,
+      'strDrinkThumb': instance.strDrinkThumb,
+      'idDrink': instance.idDrink,
+    };
