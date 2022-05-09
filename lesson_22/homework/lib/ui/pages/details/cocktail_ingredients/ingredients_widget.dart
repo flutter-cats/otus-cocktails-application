8fe5cc46b95fd9f6bafd9c53d6f7f937634ea758
Igor Kim <igor.skh@gmail.com>
2022-05-09 11:16:53 +0200

    fix deprecations

--- lesson_22/homework/lib/ui/pages/details/cocktail_ingredients/ingredients_widget.dart
+++ lesson_22/homework/lib/ui/pages/details/cocktail_ingredients/ingredients_widget.dart
@@ -1,5 +1,4 @@
 import 'package:cocktail_app/core/src/model/ingredient_definition.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'cocktail_ingredient_row.dart';
