ca6b1daa3740da849f5eb691ffe544ecee2546ef
ibacharnikov <VTB4049867>
2022-05-25 21:59:06 +0500

    Unit test passed

--- lesson_25/homework/test/repository_test.dart
+++ lesson_25/homework/test/repository_test.dart
@@ -1,5 +1,6 @@
 import 'package:cocktail_app_tests/core/models.dart';
 import 'package:http/http.dart' as http;
+import 'async_repository.dart';
 import 'mock.dart';
 import 'repository_test.mocks.dart';
 import 'package:mockito/annotations.dart';
@@ -10,14 +11,22 @@ import 'package:test/test.dart';
 // Create new instances of this class in each test.
 @GenerateMocks([http.Client])
 void main() {
-  AsyncCocktailRepository repository = AsyncCocktailRepository();
+  final client = MockClient();
+  AsyncRepository repository = AsyncRepository(client: client);
+
   group('Repo group test', () {
+
     test('Lookup cocktail should return Cocktail', () async {
-      final client = MockClient();
-      when(client.get(Uri.parse('https://the-cocktail-db.p.rapidapi.com/lookup.php?i=1')))
-          .thenAnswer((_) async => http.Response('{"drinks":[]}', 200));
-      expect(await repository.fetchCocktailDetails('1'), isA<Cocktail>());
+      when(client.get(Uri.parse('https://the-cocktail-db.p.rapidapi.com/lookup.php?i=1'), headers: headers))
+          .thenAnswer((_) async => http.Response(mockCocktail, 200));
+      final cocktail = await repository.fetchCocktailDetails('1');
+      expect(cocktail, isA<Cocktail>());
     });
 
+    test('Should caught an exception', () async {
+      when(client.get(Uri.parse('https://the-cocktail-db.p.rapidapi.com/lookup.php?i=1'), headers: headers))
+          .thenAnswer((_) async => http.Response('Not found', 404));
+      expect(repository.fetchCocktailDetails('1'), throwsException);
+    });
   });
 }
