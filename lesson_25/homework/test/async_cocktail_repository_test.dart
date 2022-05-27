035e81a5eddb9d58a5fd3d932d9bc15a7fcace85
Igor Kim <igor.skh@gmail.com>
2022-05-27 21:44:14 +0200

    test AsyncCocktailRepository for correct format and exceptions

--- lesson_25/homework/test/async_cocktail_repository_test.dart
+++ lesson_25/homework/test/async_cocktail_repository_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cocktail_app_tests/core/models.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
@@ -25,19 +27,193 @@ void main() {
     _headers = {'x-rapidapi-key': 'test'};
   });
 
+  group('Initializer', () {
+    test('init with standart client', () {
+      final repository = AsyncCocktailRepository(
+          delayDuration: 0, baseURL: "http://localhost1");
+      expect(repository.baseURL, "http://localhost1");
+      expect(repository.delayDuration, 0);
+    });
+  });
+
   group('fetchCocktailDetails', () {
-    test('returns a Cocktail if the http call completes successfully',
-        () async {
+    test('returns a Cocktail', () async {
+      const id = "17060";
+      when(
+        _client.get(
+          Uri.parse('http://localhost/lookup.php?i=$id'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(json_dump_cocktail_details, 200),
+      );
+      var cocktail = await _repository.fetchCocktailDetails(id);
+      expect(cocktail, isNotNull);
+      expect(cocktail!.id, id);
+      expect(cocktail, isA<Cocktail>());
+    });
+
+    test('returns an empty response', () async {
+      const id = "17060";
+      when(
+        _client.get(
+          Uri.parse('http://localhost/lookup.php?i=foobar'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response('', 200),
+      );
+      expect(
+        _repository.fetchCocktailDetails("foobar"),
+        throwsA(isA<FormatException>()),
+      );
+    });
+
+    test('request fails', () async {
       when(
         _client.get(
           Uri.parse('http://localhost/lookup.php?i=1'),
           headers: _headers,
         ),
       ).thenAnswer(
-        (_) async => http.Response(json_dump_cocktail_details, 200),
+        (_) async => http.Response('', 404),
       );
 
-      expect(await _repository.fetchCocktailDetails("1"), isA<Cocktail>());
+      expect(
+        _repository.fetchCocktailDetails("1"),
+        throwsA(isA<HttpException>()),
+      );
+    });
+  });
+
+  group('fetchCocktailsByCocktailCategory', () {
+    test('returns CocktailDefinition Iterable', () async {
+      when(
+        _client.get(
+          Uri.parse('http://localhost/filter.php?c=Cocktail'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(json_dump_cocktailsBycategory, 200),
+      );
+
+      expect(
+          await _repository
+              .fetchCocktailsByCocktailCategory(CocktailCategory.cocktail),
+          isA<Iterable<CocktailDefinition>>());
+    });
+
+    test('request fails', () async {
+      when(
+        _client.get(
+          Uri.parse('http://localhost/filter.php?c=Cocktail'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response('', 404),
+      );
+
+      expect(
+        _repository.fetchCocktailsByCocktailCategory(CocktailCategory.cocktail),
+        throwsA(isA<HttpException>()),
+      );
+    });
+  });
+
+  group('getRandomCocktail', () {
+    test('returns a Cocktail', () async {
+      when(
+        _client.get(
+          Uri.parse('http://localhost/random.php'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(json_dump_cocktail_details, 200),
+      );
+      var cocktail = await _repository.getRandomCocktail();
+      expect(cocktail, isNotNull);
+      expect(cocktail, isA<Cocktail>());
+    });
+
+    test('request fails', () async {
+      when(
+        _client.get(
+          Uri.parse('http://localhost/random.php'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response('', 404),
+      );
+
+      expect(
+        _repository.getRandomCocktail(),
+        throwsA(isA<HttpException>()),
+      );
+    });
+  });
+
+  group('fetchPopularCocktails', () {
+    test('returns an iterable Cocktail', () async {
+      when(
+        _client.get(
+          Uri.parse('http://localhost/popular.php'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(json_dump_popularCokctails, 200),
+      );
+      final cocktails = await _repository.fetchPopularCocktails();
+      // expect(cocktails, isNotNull);
+      expect(cocktails, isA<Iterable<Cocktail>>());
+    });
+
+    test('request fails', () async {
+      when(
+        _client.get(
+          Uri.parse('http://localhost/popular.php'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response('', 404),
+      );
+
+      expect(
+        _repository.fetchPopularCocktails(),
+        throwsA(isA<HttpException>()),
+      );
+    });
+  });
+
+  group('fetchCocktailsByCocktailType', () {
+    test('returns an iterable CocktailDefinition', () async {
+      when(
+        _client.get(
+          Uri.parse('http://localhost/filter.php?a=Alcoholic'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response(json_dump_cocktailsBycategory, 200),
+      );
+      final cocktails = await _repository
+          .fetchCocktailsByCocktailType(CocktailType.alcoholic);
+      expect(cocktails, isNotNull);
+      expect(cocktails, isA<Iterable<CocktailDefinition>>());
+    });
+
+    test('request fails', () async {
+      when(
+        _client.get(
+          Uri.parse('http://localhost/filter.php?a=Alcoholic'),
+          headers: _headers,
+        ),
+      ).thenAnswer(
+        (_) async => http.Response('', 404),
+      );
+
+      expect(
+        _repository.fetchCocktailsByCocktailType(CocktailType.alcoholic),
+        throwsA(isA<HttpException>()),
+      );
     });
   });
 }
